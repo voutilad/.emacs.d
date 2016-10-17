2bb03c107369d5772b822d4155418bb2dec06fcf
Dave Voutila <voutilad@gmail.com>
2016-10-17 13:19:36 -0400

    fix rainbows

--- customizations/editing.el
+++ customizations/editing.el
@@ -52,7 +52,9 @@
 (global-set-key (kbd "C-;") 'toggle-comment-on-line)
 
 ;; yay rainbows!
-(global-rainbow-delimiters-mode t)
+;;(global-rainbow-delimiters-mode t)
+(add-hook 'prog-mode-hoke #'rainbow-delimiters-mode)
+
 
 ;; use 2 spaces for tabs
 (defun die-tabs ()
